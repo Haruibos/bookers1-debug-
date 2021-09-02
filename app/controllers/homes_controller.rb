080900908cfb00ab363ef5009c15b01bb5cb40b1
Haruibos <sunada.381724@gmail.com>
2021-09-02 07:05:30 +0000

    Finish

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,14 @@
-class Controller < ApplicationController
-  def top
+class HomesController < ApplicationController
+  def index
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
 end
